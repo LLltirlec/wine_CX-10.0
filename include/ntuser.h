b1f395d84882f59f15504c2e984bf27da8be84e2
LLltirlec <fhntvrf1998@gmail.com>
2025-01-08 14:47:05 +0300

    Update ntuser.h

--- include/ntuser.h
+++ include/ntuser.h
@@ -25,6 +25,7 @@
 #include <immdev.h>
 #include <shellapi.h>
 #include <winternl.h>
+#include "shellscalingapi.h"
 
 #ifndef W32KAPI
 # if defined(_WIN32U_) || defined(WINE_UNIX_LIB)
@@ -35,13 +36,13 @@
 #endif
 
 /* avoid including shellscalingapi.h */
-typedef enum MONITOR_DPI_TYPE
-{
-    MDT_EFFECTIVE_DPI   = 0,
-    MDT_ANGULAR_DPI     = 1,
-    MDT_RAW_DPI         = 2,
-    MDT_DEFAULT         = MDT_EFFECTIVE_DPI,
-} MONITOR_DPI_TYPE;
+// typedef enum MONITOR_DPI_TYPE
+// {
+//     MDT_EFFECTIVE_DPI   = 0,
+//     MDT_ANGULAR_DPI     = 1,
+//     MDT_RAW_DPI         = 2,
+//     MDT_DEFAULT         = MDT_EFFECTIVE_DPI,
+// } MONITOR_DPI_TYPE;
 
 typedef NTSTATUS (WINAPI *ntuser_callback)( void *args, ULONG len );
 NTSYSAPI NTSTATUS KeUserModeCallback( ULONG id, const void *args, ULONG len, void **ret_ptr, ULONG *ret_len );
